e01e85221ef945a7139555b37133f3dd5734ccdf
PrzemyslawZnojek <przemek.znojek@gmail.com>
2023-08-22 00:38:48 +0200

    Add single digit for minutes formatting

--- src/main/java/org/example/constant/Preposition.java
+++ src/main/java/org/example/constant/Preposition.java
@@ -4,4 +4,6 @@ public class Preposition {
     public static final String PAST = " past ";
     public static final String TO = " to ";
     public static final String WHITESPACE = " ";
+
+    public static final String OH = " oh ";
 }
