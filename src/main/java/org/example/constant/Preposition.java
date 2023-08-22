60a44518f19b37701231f543df22c078616e35da
PrzemyslawZnojek <przemek.znojek@gmail.com>
2023-08-22 23:54:38 +0200

    Remove whitespaces

--- src/main/java/org/example/constant/Preposition.java
+++ src/main/java/org/example/constant/Preposition.java
@@ -4,6 +4,5 @@ public class Preposition {
     public static final String PAST = " past ";
     public static final String TO = " to ";
     public static final String WHITESPACE = " ";
-
     public static final String OH = " oh ";
 }
